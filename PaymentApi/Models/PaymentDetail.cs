525e1bf1cf20e490dd3850b1d9a311ace77cbe7e
Baumel Márton <marcibaumel@gmail.com>
2021-10-14 12:00:05 +0200

    Add project files.

---
+++ PaymentApi/Models/PaymentDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentApi.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetilId { get; set; }
+        public string CardOwnerName { get; set; }
+        public string CardNumber { get; set; }
+        public string ExperitonDFate { get; set; }
+        public string SecurityCode { get; set; }
+    }
+}
